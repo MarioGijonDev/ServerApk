3e9362f87f9fe1d1d87c017a7385d9d36844fe97
Mario <gijonlietormariodev@gmail.com>
2024-06-25 14:05:49 +0200

    First commit

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ app = FastAPI()
 
 @app.get("/test")
 def index():
-  return {
-    "Recommends" : {
-      {
-        "movie" : "1011985",
-        "txt": "ESTE ES EL TEXTO DE KUNG FU PANDA"
-      },
-      {
-        "movie" : "940551",
-        "txt": "ESTE ES EL TEXTO DEL PUTO PÁJARO"
-      }
-      
+    return {
+        "Recommends": [
+            {
+                "movie": "1011985",
+                "txt": "ESTE ES EL TEXTO DE KUNG FU PANDA"
+            },
+            {
+                "movie": "940551",
+                "txt": "ESTE ES EL TEXTO DEL PUTO PÁJARO"
+            }
+        ]
     }
-    
-  }
 

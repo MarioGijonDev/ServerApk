6bd0fe82480820971a3ad29f934f5fa7a0484990
Mario <gijonlietormariodev@gmail.com>
2024-06-25 14:40:55 +0200

    First commit

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def index():
     return {
         "Recommends": [
             {
-                "movie": "1011985",
+                "id": "1011985",
                 "txt": "ESTE ES EL TEXTO DE KUNG FU PANDA"
             },
             {
-                "movie": "940551",
+                "id": "940551",
                 "txt": "ESTE ES EL TEXTO DEL PUTO PÁJARO"
             }
         ]
